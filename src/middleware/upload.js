085ee22d29df8cdeb1164691de32db3e2bfda6dc
Oleksandr Kalenichenko <62474613+sashakter@users.noreply.github.com>
2025-02-18 12:54:20 +0100

    Merge pull request #2
    * init uploads
    
    * update package.json

---
+++ src/middleware/upload.js
@@ -0,0 +1,31 @@
+import multer from "multer";
+import path from "path";
+
+// Настраиваем хранилище
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        cb(null, Date.now() + ext);
+    },
+});
+
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only images are allowed!"), false);
+    }
+};
+
+// Ограничения размера
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+});
+
+export default upload;
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,8 @@
 import express from "express";
 import {creationPostController, getAllPostsController} from "../controllers/posts.js";
-
+import upload from '../middleware/upload.js'
 const router = express.Router();
 
 router.get('/', getAllPostsController);
-router.post('/', creationPostController);
-
+router.post("/", upload.array("images", 5), creationPostController);
 export default router;
