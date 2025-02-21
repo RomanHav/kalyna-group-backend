64cafa10d2efbbd3b8e913efbd0559af3ec447f0
Oleksandr Kalenichenko <62474613+sashakter@users.noreply.github.com>
2025-02-21 21:57:17 +0100

    Merge pull request #14
    * update post

--- src/db/models/posts.js
+++ src/db/models/posts.js
@@ -4,6 +4,6 @@ const postsSchema = new mongoose.Schema({
     images: [],
     description: String,
     link: String,
-});
+}, { timestamps: true });
 
 export const PostsCollection = mongoose.model('Posts', postsSchema);
