88ad2c48938b00664fd54fe683d0bb9e5d22e325
sashakter <sashakter@gmail.com>
2025-02-22 21:20:23 +0100

    upload.js edit file size

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -27,7 +27,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage,
     fileFilter,
-    limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10 mb
 });
 
 export default upload;
